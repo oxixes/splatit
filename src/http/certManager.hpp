e6f438a03b1863b06218c8e8400a399b62780c00
oxixes <oxixes@protonmail.com>
2023-01-28 03:35:36 +0100

    Libs set up.

--- src/http/certManager.hpp
+++ src/http/certManager.hpp
@@ -1,5 +1,7 @@
 #ifndef SPLATOON_SERVER_CERTMANAGER_HPP
 #define SPLATOON_SERVER_CERTMANAGER_HPP
 
+#include <jwt-cpp/jwt.h>
+#include <nlohmann/json.hpp>
 
 #endif //SPLATOON_SERVER_CERTMANAGER_HPP
